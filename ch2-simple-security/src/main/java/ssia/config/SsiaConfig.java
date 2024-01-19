7e6021062e4a8c58f63426618afbb6d899b1e510
Yu Li <yu.li@tecsys.com>
2024-01-19 11:13:36 -0500

    Config AuthenticationProvider directly

--- ch2-simple-security/src/main/java/ssia/config/SsiaConfig.java
+++ ch2-simple-security/src/main/java/ssia/config/SsiaConfig.java
@@ -5,43 +5,27 @@ package ssia.config;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import ssia.security.SsiaAuthenticationProvider;
 
 
 @Configuration
+@ComponentScan(basePackages = {"/ssia/controllers", "/ssia/security"})
 class SsiaConfig {
 
     @Bean
-    UserDetails user() {
-        return User.withUsername("yul")
-                .password("password")
-                .authorities("read")
-                .build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(UserDetails user) {
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    AuthenticationProvider authenticationProvider() {
+        return new SsiaAuthenticationProvider();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.httpBasic();
-
         http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
 
         // Makes all the endpoints accessible without the need for credentials
---
+++ ch2-simple-security/src/main/java/ssia/security/SsiaAuthenticationProvider.java
@@ -0,0 +1,41 @@
+//: ssia.security.SsiaAuthenticationProvider.java
+
+
+package ssia.security;
+
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+
+public class SsiaAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication)
+            throws AuthenticationException {
+
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+
+        if ("yul".equals(username) && "password".equals(password)) {
+            return new UsernamePasswordAuthenticationToken(
+                    username, password, Arrays.asList());
+        } else {
+            throw new AuthenticationCredentialsNotFoundException(
+                    ">>> Authentication Failed!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(
+                authentication);
+    }
+
+}///:~
