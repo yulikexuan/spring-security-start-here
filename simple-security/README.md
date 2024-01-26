4a5b8a2e855abbcd3c4fdc41eff822a9f4543bc6
Yu Li <yu.li@tecsys.com>
2024-01-26 17:09:20 -0500

    Access SecurityContext instance in controllers

--- simple-security/README.md
+++ simple-security/README.md
@@ -4,5 +4,7 @@
 
 ### Call with Predefined Credential
 - ` curl  -v -w '\nStatus: %{http_code}\nTotal: %{time_total} s\n' GET -H "Content-Type: application/json" -u yul:yul http://localhost:7070/greeting/hello `
+- ` curl  -v -w '\nStatus: %{http_code}\nTotal: %{time_total} s\n' GET -H "Content-Type: application/json" -u yul:yul http://localhost:7070/greeting/bonjour `
 - ` curl  -v -w '\nStatus: %{http_code}\nTotal: %{time_total} s\n' GET -H "Content-Type: application/json" -u joel:joel http://localhost:7070/greeting/hello `
+- ` curl  -v -w '\nStatus: %{http_code}\nTotal: %{time_total} s\n' GET -H "Content-Type: application/json" -u joel:joel http://localhost:7070/greeting/bonjour `
 - ` curl  -v -w '\nStatus: %{http_code}\nTotal: %{time_total} s\n' GET -H "Content-Type: application/json" -u berry:berry http://localhost:7070/greeting/hello `
--- simple-security/src/main/java/ssia/web/controller/HelloController.java
+++ simple-security/src/main/java/ssia/web/controller/HelloController.java
@@ -1,6 +1,9 @@
 package ssia.web.controller;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +16,16 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
 
-        return "Hello!";
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+
+        Authentication authentication = securityContext.getAuthentication();
+
+        return "Hello %s !  ".formatted(authentication.getName());
+    }
+
+    @GetMapping("/bonjour")
+    public String bonjour(Authentication authentication) {
+        return "Bonjour %s !  ".formatted(authentication.getName());
     }
 
 }
